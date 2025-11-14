f10f6a2320e3cddf46ee563ef10dfc09b273da5c
rayanrzn <akofry@gmail.com>
2025-11-14 01:50:58 +0000

    Projet  IA Fini RZN

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ import pandas as pd
 
 données = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSC4KusfFzvOsr8WJRgozzsCxrELW4G4PopUkiDbvrrV2lg0S19-zeryp02MC9WYSVBuzGCUtn8ucZW/pub?output=csv')
 
-figure = px.pie(données, values='qte', names='region', title='quantité vendue par région')
+données['ca'] = données['qte'] * données['prix']
 
-figure.write_html('ventes-par-region.html')
+figure = px.pie(données, values='ca', names='produit', title="Chiffre d'affaires par produit")
 
-print('ventes-par-région.html généré avec succès !')
+figure.write_html('ca-par-produit.html')
+
+print("ca-par-produit.html généré avec succès !")
